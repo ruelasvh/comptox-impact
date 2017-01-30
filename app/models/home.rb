53a3fdd7af3cad9c050dab2d0a889c2757d64209
vruelasr <ruelas-rivera.victor@epa.gov>
2017-01-30 14:07:13 -0500

    [CTI-105] Validation and unit testing of Home model.

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,3 +1,7 @@
 class Home < ActiveRecord::Base
-  validates :title, :intro, presence: true
+  validates :title, :intro, :scientists_img_url, :publications_img_url, :datatools_img_url, :impact_img_url, presence: true
+  validates :scientists_img_url, :publications_img_url, :datatools_img_url, :impact_img_url, allow_blank: true, format: {
+      with: %r{\.(gif|jpg|png)\Z}i,
+      message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
--- test/models/home_test.rb
+++ test/models/home_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class HomeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :homes
+
+  test "home attributes must not be empty" do
+    home = Home.new
+    assert home.invalid?
+    assert home.errors[:title].any?
+    assert home.errors[:intro].any?
+    assert home.errors[:scientists_img_url].any?
+    assert home.errors[:publications_img_url].any?
+    assert home.errors[:datatools_img_url].any?
+    assert home.errors[:impact_img_url].any?
+  end
+
+  def new_home(image_url)
+    Home.new( title:                "Impact Title",
+              intro:                "NCCT wide metrics.",
+              scientists_img_url:   image_url,
+              publications_img_url: image_url,
+              datatools_img_url:    image_url,
+              impact_img_url:       homes(:rev_three).impact_img_url)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    ok.each do |name|
+      assert new_home(name).valid?, "#{name} should be valid"
+    end
+    bad.each do |name|
+      assert new_home(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
 end
