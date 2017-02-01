9d8887b785b1e370c1999df05f27003c47830496
vruelasr <ruelas-rivera.victor@epa.gov>
2017-02-01 15:37:34 -0500

    Added validation and unit testing

--- app/models/scientist.rb
+++ app/models/scientist.rb
@@ -1,6 +1,7 @@
 class Scientist < ActiveRecord::Base
   self.primary_key = "scientistId"
   validates :scientistId, :firstName, :lastName, :title, presence: true
+  validates :scientistId, uniqueness: true
   validates :photoUrl, allow_blank: true, format: {
       with: %r{\.(gif|jpg|png)\Z}i,
       message: 'must be a URL for GIF, JPG or PNG image.'
--- test/models/scientist_test.rb
+++ test/models/scientist_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class ScientistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :scientists
+
+  test "scientist attributes must not be empty" do
+    scientist = Scientist.new
+    assert scientist.invalid?
+    assert scientist.errors[:scientistId].any?
+    assert scientist.errors[:firstName].any?
+    assert scientist.errors[:lastName].any?
+    assert scientist.errors[:title].any?
+  end
+
+  def new_scientist(image_url)
+    Scientist.new(  scientistId:          31,
+                    firstName:            "Juan",
+                    lastName:             "Perez",
+                    title:                "Rocket Scientist",
+                    photoUrl:             image_url)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    ok.each do |name|
+      assert new_scientist(name).valid?, "#{name} should be valid"
+    end
+    bad.each do |name|
+      assert new_scientist(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  test "scientist is not valid without a unique scientistId" do
+    scientist = Scientist.new(  scientistId:          scientists(:ann).scientistId,
+                    firstName:            "Juan",
+                    lastName:             "Perez",
+                    title:                "Rocket Scientist",
+                    photoUrl:             "selfie.gif")
+
+    assert scientist.invalid?
+    assert_equal ["has already been taken"], scientist.errors[:scientistId]
+  end
 end
