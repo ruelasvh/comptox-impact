fd1bda16c274a5dfa000f5c42b3ae8c31659fa7f
vruelasr <ruelas-rivera.victor@epa.gov>
2017-02-01 15:47:21 -0500

    Removed warnings for columns that are null

--- frontend/features/scientistsindex/components/ScientistsItem.js
+++ frontend/features/scientistsindex/components/ScientistsItem.js
@@ -11,14 +11,12 @@ const Scientist = ({ scientist, scientists_path, i }) => (
          last-name={scientist.lastName}
          key={"scientist-component-" + i}>
         <a href={scientists_path + "/" + scientist.scientistId}>
-            {/*<Link to={`/scientists/${props.scientist.scientistId}`}>*/}
             <Thumbnail
                 src={require('../img/' + scientist.photoUrl)}
                 alt="Scientist Image">
                 <h4>{scientist.firstName + ' ' + scientist.lastName}</h4>
                 <p>{scientist.title}</p>
             </Thumbnail>
-            {/*</Link>*/}
         </a>
     </div>
 );
@@ -29,24 +27,24 @@ Scientist.propTypes = {
         firstName: PropTypes.string.isRequired,
         lastName: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
-        email: PropTypes.string.isRequired,
-        photoUrl: PropTypes.string.isRequired,
-        orcid: PropTypes.string.isRequired,
-        researchGate: PropTypes.string.isRequired,
-        googleScholar: PropTypes.string.isRequired,
-        linkedIn: PropTypes.string.isRequired,
-        plumx: PropTypes.string.isRequired,
-        publons: PropTypes.string.isRequired,
-        europePmc: PropTypes.string.isRequired,
-        bio: PropTypes.string.isRequired,
-        createdBy: PropTypes.string.isRequired,
-        updatedBy: PropTypes.string.isRequired,
-        publications: PropTypes.string.isRequired,
-        presentations: PropTypes.string.isRequired,
-        projects: PropTypes.string.isRequired,
-        selfUrl: PropTypes.string.isRequired,
-        created_at: PropTypes.string.isRequired,
-        updated_at: PropTypes.string.isRequired
+        // email: PropTypes.string.isRequired,
+        // photoUrl: PropTypes.string.isRequired,
+        // orcid: PropTypes.string.isRequired,
+        // researchGate: PropTypes.string.isRequired,
+        // googleScholar: PropTypes.string.isRequired,
+        // linkedIn: PropTypes.string.isRequired,
+        // plumx: PropTypes.string.isRequired,
+        // publons: PropTypes.string.isRequired,
+        // europePmc: PropTypes.string.isRequired,
+        // bio: PropTypes.string.isRequired,
+        // createdBy: PropTypes.string.isRequired,
+        // updatedBy: PropTypes.string.isRequired,
+        // publications: PropTypes.string.isRequired,
+        // presentations: PropTypes.string.isRequired,
+        // projects: PropTypes.string.isRequired,
+        // selfUrl: PropTypes.string.isRequired,
+        // created_at: PropTypes.string.isRequired,
+        // updated_at: PropTypes.string.isRequired
     }),
     scientists_path: PropTypes.string.isRequired,
     i: PropTypes.number.isRequired
