ab84dc234c8fd65de339c42e1a926d8cf0b9c90d
vruelasr <ruelas-rivera.victor@epa.gov>
2017-03-09 09:19:36 -0500

    Corrected validates_length_of syntax

--- app/models/publication_type.rb
+++ app/models/publication_type.rb
@@ -1,10 +1,10 @@
 class PublicationType < ActiveRecord::Base
   self.primary_key = "publication_type_id"
   validates :name, :label, :short_description, presence: true
-  validates_length_of :name, limit: 64
-  validates_length_of :label, limit: 64
-  validates_length_of :short_description, limit: 1024
-  validates_length_of :long_description, limit: 2048, allow_blank: true
-  validates_length_of :created_by, limit: 256, allow_blank: true
-  validates_length_of :updated_by, limit: 256, allow_blank: true
+  validates_length_of :name, maximum: 64
+  validates_length_of :label, maximum: 64
+  validates_length_of :short_description, maximum: 1024
+  validates_length_of :long_description, maximum: 2048, allow_blank: true
+  validates_length_of :created_by, maximum: 256, allow_blank: true
+  validates_length_of :updated_by, maximum: 256, allow_blank: true
 end
