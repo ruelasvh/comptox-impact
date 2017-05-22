46ba7e7424bf86016587d16b108623a2f2f319c7
Jon Anderson <anderson.jonathan@epa.gov>
2017-05-22 17:28:19 -0400

    Forgot to change numbering for build instructions

--- client/buildInstructions.js
+++ client/buildInstructions.js
@@ -4,10 +4,10 @@
  */
 console.warn("These are the build instructions for deployment to ag:");
 console.warn("1. Replace 'http://localhost:4000/client-app.js' with 'application' in  /app/views/layouts/application.html.erb");
-console.warn("3. Run: NODE_ENV=production webpack --config ./webpack.config.js --progress --colors");
+console.warn("2. Run: NODE_ENV=production webpack --config ./webpack.config.js --progress --colors");
 console.warn("These are the build instructions for development in localhost:");
 console.warn("1. Replace 'application' with 'http://localhost:4000/client-app.js' in  /app/views/layouts/application.html.erb");
-console.warn("3. Run: rails s");
-console.warn("4. Run: npm start");
+console.warn("2. Run: rails s");
+console.warn("3. Run: npm start");
 
 
