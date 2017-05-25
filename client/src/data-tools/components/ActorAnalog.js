5babaeba18dcff88a6abfeb7ae731c3681f6d616
Jon Anderson <anderson.jonathan@epa.gov>
2017-05-25 12:43:23 -0400

    CTI-165 Refactoring with bug fixes for ACToR Analog Stats tab

--- client/src/data-tools/components/ActorAnalog.js
+++ client/src/data-tools/components/ActorAnalog.js
@@ -3,52 +3,57 @@ import { FormControl, Button } from 'react-bootstrap'
 import moment from 'moment'
 import { connect } from 'react-redux';
 
-const years = [ '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017' ];
-const yearsRev = [].concat(years).reverse();
-const months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
-let dateObj = {};
+let analogStart = moment('2009-9-1')
+let analogEnd = moment()
+let dates = []
+let months = []
+let year = {}
+let month = ''
+
+while (analogEnd > analogStart) {
+  let year = dates.filter((year) => year.year === analogStart.year())[0]
+  if(!year) {
+    year = { year: analogStart.year(), months: [] }
+    dates.push(year)
+  }
+  year.months.push(analogStart.format('MMMM'))
+  analogStart.add(1,'month')
+}
+dates.reverse()
+year = dates[0]
+months = year.months
+month = months[months.length - 1]
 
 class ActorAnalog extends React.Component {
   constructor() {
     super();
-    this.state = {months: [], year: yearsRev[0], month: 0};
+
+    this.state = { dates, months, month, year };
 
     this.loadYear = this.loadYear.bind(this);
     this.loadMonth = this.loadMonth.bind(this);
   }
   loadYear(e) {
-    this.setState({year: e.target.value, month: 0, months: dateObj[e.target.value].map(month => months[month])});
+    let year = dates.filter((year) => year.year === parseInt(e.target.value))[0]
+    let months = year.months
+    let month = months[0]
+    this.setState({ year, months, month })
   }
   loadMonth(e) {
-    this.setState({month: months.indexOf(e.target.value)});
-  }
-  componentDidMount() {
-    let month = 8;
-    for (let i = 0; i < years.length; month++) {
-      if(!dateObj.hasOwnProperty(years[i])) {
-        dateObj[years[i]] = [];
-      }
-      if(years[i] === '2017' && month > 4) {
-        break;
-      }
-      dateObj[years[i]].push(month);
-      if(month === 11) {
-        month = -1;
-        i++;
-      }
-    }
-    this.setState({month: dateObj[yearsRev[0]].length - 1, months: dateObj[yearsRev[0]].map(month => months[month])});
+    let month = e.target.value
+    this.setState({ month })
   }
   render() {
-    let src = 'https://actor.epa.gov/reports/' + months[this.state.month] + '_' + this.state.year + '.html';
+    let src = 'https://actor.epa.gov/reports/' + this.state.month + '_' + this.state.year.year + '.html';
     return (
       <div>
       <FormControl componentClass="select" onChange={this.loadYear}>
-      {yearsRev.map((year) => <option value={year} key={year}>{year}</option>)}
+      {this.state.dates.map((year) => <option value={year.year} key={year.year}>{year.year}</option>)}
       </FormControl>
       <div className="buttonContainer">
       {this.state.months.map((month) => <Button value={month} onClick={this.loadMonth} key={month}>{month}</Button>)}
       </div>
+      <br/>
       <iframe style={{width: '100%', height: '400px'}} src={src} />
       </div>
     );
