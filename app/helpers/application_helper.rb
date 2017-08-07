1e450395fb4d20c61fcebab02719ee8a94cc062d
vruelasr <ruelas-rivera.victor@epa.gov>
2017-08-07 13:07:46 -0400

    Added new environment development_webpack_dev_server to avoid possible merge conflicts with app/views/layouts/application.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def load_client_app
+    if Rails.env == 'development_webpack_dev_server'
+      javascript_include_tag 'http://localhost:4000/client-app.js'
+    else
+      javascript_include_tag 'application'
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module ComptoxImpact
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Disable generation of controller specific asset files
+    config.generators do |g|
+      g.assets false
+    end
   end
 end
